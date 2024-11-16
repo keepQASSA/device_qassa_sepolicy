LineageOS/android_device_lineage_sepolicy

atv: Allow setupwraith configs to be set by vendor
Change-Id: I0e2831019d5bbb9d432bbad48cd91c98f3c3cb6f

atv/sepolicy.mk
@@ -1,0 +1,9 @@
+#
+# This policy configuration will be used by atv products that
+# inherit from Lineage
+#
+
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/atv/vendor
+endif

keepQASSA/device_qassa_sepolicy

Signed-off-by: rayrzy <vincentrayananda@gmail.com>

atv/sepolicy.mk
@@ -1,0 +1,9 @@
+#
+# This policy configuration will be used by atv products that
+# inherit from Lineage
+#
+
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_SEPOLICY_DIRS += \
+    device/qassa/sepolicy/atv/vendor
+endif